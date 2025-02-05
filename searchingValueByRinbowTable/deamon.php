47fbcbef1569c2794b27e3e64ef4f537f2379cd6
ElkaMaclaud <elkamaclaud@gmail.com>
2025-02-05 14:21:56 +0300

    fix: fixed bug when saving to redis

--- searchingValueByRinbowTable/deamon.php
+++ searchingValueByRinbowTable/deamon.php
@@ -2,9 +2,8 @@
 $redis = new Redis();
 $redis->connect('127.127.126.42', 6379);
 
-// Функция редукции (преобразование хэша обратно в строку)
+// Функция редукции
 function reduceHash($hash, $iteration) {
-    // Пример редукции: берём первые 8 символов хэша и интерпретируем их как строку
     $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     $reduced = '';
     for ($i = 0; $i < 8; $i++) {
@@ -14,16 +13,24 @@ function reduceHash($hash, $iteration) {
     return $reduced;
 }
 
-// Добавляем тестовую запись "hello" в Redis
-$testString = "hello";
-$testHash = hash('sha256', $testString);
-$redis->setex($testHash, 3600, $testString); // Сохраняем пару хэш -> строка с TTL 1 час
-echo "Test entry added: $testHash -> $testString\n";
-
-// Генерация цепочек
+// Генерация радужной таблицы
 function generateRainbowTable($chainLength = 1000, $numChains = 100) {
     global $redis;
 
+    // Добавляем слово "hello" в таблицу
+    $testString = "hello";
+    $currentString = $testString;
+
+    for ($j = 0; $j < $chainLength; $j++) {
+        $hash = hash('sha256', $currentString);
+        $currentString = reduceHash($hash, $j);
+    }
+
+    // Сохраняем начало и конец цепочки для "hello"
+    $redis->setex($currentString, 3600, $testString);
+    echo "Test chain added: $testString -> $currentString\n";
+
+    // Генерируем остальные цепочки
     for ($i = 0; $i < $numChains; $i++) {
         $startString = substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"), 0, 8);
         $currentString = $startString;
@@ -34,7 +41,7 @@ function generateRainbowTable($chainLength = 1000, $numChains = 100) {
         }
 
         // Сохраняем начало и конец цепочки в Redis
-        $redis->setex($startString, 3600, $currentString);
+        $redis->setex($currentString, 3600, $startString);
         echo "Chain generated: $startString -> $currentString\n";
     }
 }
